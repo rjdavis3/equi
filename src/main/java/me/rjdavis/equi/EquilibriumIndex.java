9d4f27da563a01b2f628d233d3986c52e5ac4a11
R.J. Davis <rj@rjdavis.me>
2018-08-10 06:31:06 -0400

    Removed prefixSum stream to increase performance.

--- src/main/java/me/rjdavis/equi/EquilibriumIndex.java
+++ src/main/java/me/rjdavis/equi/EquilibriumIndex.java
@@ -9,9 +9,12 @@ public class EquilibriumIndex {
 	}
 
 	public static int find(List<Integer> list) {
+		long prefixSum = 0;
 		for (int i = 0; i < list.size(); i++) {
-			long prefixSum = list.subList(0, i).stream().mapToLong(Long::valueOf).sum();
-			long suffixSum = (i == list.size() - 1) ? 0
+			if (i > 0) {
+				prefixSum += list.get(i - 1);
+			}
+			final long suffixSum = (i == list.size() - 1) ? 0
 					: list.subList(i + 1, list.size()).stream().mapToLong(Long::valueOf).sum();
 			if (prefixSum == suffixSum) {
 				return i;
